dde7979a9ada6f1a020e941259d23b0a66779362
Wanvisa Phumam <wanvisaphumam@maneko.local>
2025-05-15 15:24:05 +0700

    Initial commit

---
+++ src/main/java/org/femgev/pancake/Pancake.java
@@ -0,0 +1,17 @@
+package org.femgev.pancake;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Pancake extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
